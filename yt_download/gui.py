ac4680cf0ba6222aab433d3a81108d550d4e4cca
unknown <leeab1310@gmail.com>
2024-01-03 22:30:27 +0800

    pytube usage

---
+++ yt_download/gui.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import tkinter as tk
+import time
+import threading
+import random
+from pytube_usage import str_to_ytlink, download_list
+
+class MyFrame(tk.Frame):
+    def __init__(self, root):
+        tk.Frame.__init__(self, root, background="red")
+        self.l1 = tk.Label(self, text="輸入網址:", relief='solid', borderwidth=1)
+        self.l1.pack(expand=True, fill=tk.BOTH, padx=1, pady=1)
+
+        self.t1 = tk.Text(self, width=5)
+        self.t1.pack(expand=True, fill=tk.BOTH)
+
+        self.b1 = tk.Button(self, text="下載YT(mp3)", command=self.analyse)
+        self.b1.pack(expand=True, fill=tk.BOTH, pady=5)
+        
+        self.b2 = tk.Button(self, text="下載YT(mp4)", command=self.analyse2)
+        self.b2.pack(expand=True, fill=tk.BOTH, pady=5)
+
+        # self.result = tk.Label(self, text="點擊上面分析")
+        # self.result.pack(expand=True, fill=tk.BOTH)
+
+    def analyse(self):
+        def work():
+            self.b1["state"] = tk.DISABLED
+            time.sleep(1)
+            text = self.t1.get("1.0", "end")
+            
+            path = r'.\music'
+            yt_list = str_to_ytlink(text)
+            download_list(yt_list, path, True)
+            
+            self.b1["state"] = tk.ACTIVE
+        thread = threading.Thread(target=work)
+        thread.start()
+        
+    def analyse2(self):
+        def work():
+            self.b2["state"] = tk.DISABLED
+            time.sleep(1)
+            text = self.t1.get("1.0", "end")
+            
+            path = r'.\music'
+            yt_list = str_to_ytlink(text)
+            download_list(yt_list, path, False)
+            
+            self.b2["state"] = tk.ACTIVE
+        thread = threading.Thread(target=work)
+        thread.start()
+
+if __name__=='__main__':
+    window = tk.Tk()
+    window.geometry("500x500+200+200")
+    
+    f1 = MyFrame(window)
+    f1.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=20, pady=20)
+    
+    window.mainloop()
